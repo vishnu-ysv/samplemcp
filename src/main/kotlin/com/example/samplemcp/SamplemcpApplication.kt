5f612b1e00cd4f222309ccf3d84607071b310e05
srivishnu.y <srivishnu.yarlagadda@superops.ai>
2025-06-26 18:32:35 +0530

    first commit

---
+++ src/main/kotlin/com/example/samplemcp/SamplemcpApplication.kt
@@ -0,0 +1,11 @@
+package com.example.samplemcp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SamplemcpApplication
+
+fun main(args: Array<String>) {
+	runApplication<SamplemcpApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/samplemcp/SamplemcpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.samplemcp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SamplemcpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
