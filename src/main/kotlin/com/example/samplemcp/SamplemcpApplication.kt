2db82a699d019d42539a99d419020613c3d5643e
srivishnu.y <srivishnu.yarlagadda@superops.ai>
2025-07-08 22:57:33 +0530

    sample mcp

--- src/main/kotlin/com/example/samplemcp/SamplemcpApplication.kt
+++ src/main/kotlin/com/example/samplemcp/SamplemcpApplication.kt
@@ -18,4 +18,4 @@ fun main(args: Array<String>) {
 @Bean
 fun sampleMCPService(sampleMCPService: SampleMCPService) : ToolCallbackProvider {
     return MethodToolCallbackProvider.builder().toolObjects(sampleMCPService).build()
-}-
+}
